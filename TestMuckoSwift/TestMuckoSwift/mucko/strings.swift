c9b551a8c8beaa32241f08fa5ba66686b322504a
WooKyoung Noh <wookay.noh@gmail.com>
2018-12-18 06:58:57 +0900

    TestString

--- TestMuckoSwift/TestMuckoSwift/mucko/strings.swift
+++ TestMuckoSwift/TestMuckoSwift/mucko/strings.swift
@@ -14,7 +14,14 @@ class TestString: WTestCase {
     }
 
     @objc func test_repr() {
+        Assert.equal(repr(3.14), "3.14")
+        Assert.equal(repr(-3.14), "-3.14")
+        Assert.equal(repr("a"), "\"a\"")
         Assert.equal(repr(["a"]), "[\"a\"]")
+    }
+
+    @objc func test_string() {
+        Assert.equal(string(1,2,3,true), "123true")
     }
 
 }
